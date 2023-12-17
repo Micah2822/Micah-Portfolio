4cb3da5370373ab4ff423682dc29904398450b83
Micah2822 <127398262+Micah2822@users.noreply.github.com>
2023-12-17 22:13:51 +0000

    Update evaluation metrics in model_evaluation.py

--- Boxing Match Outcome Prediction/Fight_Prediction_Model/NN_ML_Model/model_evaluation.py
+++ Boxing Match Outcome Prediction/Fight_Prediction_Model/NN_ML_Model/model_evaluation.py
@@ -1,11 +1,8 @@
 
 
-from Fight_Prediction_Model.data_processing.data_select_clean import x_train, x_test, x_cv, y_cv, y_test, y_train, dataset
+from Fight_Prediction_Model.data_processing.data_select_clean import x_train, x_test, x_cv, y_cv, y_test, y_train
 from Fight_Prediction_Model.NN_ML_Model.nn_model import model
-import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 from sklearn.metrics import mean_squared_error
 import tensorflow as tf
 
@@ -18,17 +15,20 @@ y_test = tf.convert_to_tensor(y_test, dtype=tf.float32)
 x_cv = tf.convert_to_tensor(x_cv, dtype=tf.float32)
 y_cv = tf.convert_to_tensor(y_cv, dtype=tf.float32)
 
-# Compute the test MSE
-yhat_test = model.predict(x_test)
-test_mse = mean_squared_error(y_test, yhat_test) / 2
-print("Test MSE: ", test_mse)
+# Predict using the model
+yhat_test = model.predict(x_test).flatten()
+yhat_test = [1 if y > 0.5 else 0 for y in yhat_test]  # Convert probabilities to class labels
 
-# Record the training MSE
-yhat_training = model.predict(x_train)
-train_mse = mean_squared_error(y_train, yhat_training) / 2
-print("Train MSE: ", train_mse)
+# Calculate evaluation metrics
+accuracy = accuracy_score(y_test, yhat_test)
+precision = precision_score(y_test, yhat_test)
+recall = recall_score(y_test, yhat_test)
+f1 = f1_score(y_test, yhat_test)
+roc_auc = roc_auc_score(y_test, yhat_test)
 
-# Record the cross validation MSEs
-yhat_cv = model.predict(x_cv)
-cv_mse = mean_squared_error(y_cv, yhat_cv) / 2
-print("CV MSE: ", cv_mse)
+# Print the metrics
+print(f"Accuracy: {accuracy:.4f}")
+print(f"Precision: {precision:.4f}")
+print(f"Recall: {recall:.4f}")
+print(f"F1 Score: {f1:.4f}")
+print(f"AUC-ROC: {roc_auc:.4f}")
