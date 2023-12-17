e8d31a7f9f32aca214a707078f9740d8b0411c7a
Micah2822 <127398262+Micah2822@users.noreply.github.com>
2023-12-17 22:58:40 +0000

    Update data_select_clean.py

--- Boxing Match Outcome Prediction/Fight_Prediction_Model/data_processing/data_select_clean.py
+++ Boxing Match Outcome Prediction/Fight_Prediction_Model/data_processing/data_select_clean.py
@@ -1,29 +1,23 @@
-import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv("~/Fight_Prediction_Model/dataset/dataset.csv")
-
-
+dataset = pd.read_csv("https://github.com/Micah2822/Micah-Portfolio/blob/main/Boxing%20Match%20Outcome%20Prediction/Fight_Prediction_Model/dataset/dataset.csv")
 
 dataset = dataset[dataset['clean_outcome'].notna()]
 dataset = dataset[dataset['clean_outcome'] != 'Unknown']
+
+#categorical to numerical
 dataset['clean_outcome'] = dataset['clean_outcome'].map({'win': 1, 'loss': 0})
 
-
-X = dataset[['opp_last6','opp_KO ratio','Win KO','opp_winOther','Loss Other','Loss KO','Win Other','KO ratio','opp_winOther',
-             'KnockedOut ratio','opp_winKO','opp_KO ratio','opp_loss','opp_lossOther','opp_win','Draw','opp_lossKO']]
+X = dataset[['opp_last6', 'opp_KnockedOut ratio', 'opp_winOther',  'opp_winKO', 'opptotalaccuracy', 'oppavgtotalagainst',
+                    'opp_KO ratio', 'opp_loss', 'opp_lossOther', 'opp_win', 'opp_lossKO', 'oppavgtotallanded',
+                    'Total punch accuracy', 'Avg Total punches landed', 'Avg Total punches landed against',
+                    'Win KO', 'Loss KO', 'Win Other', 'KO ratio', 'Loss Other',
+                    'KnockedOut ratio', 'Draw', 'last6']]
 X = X.fillna(0)
 
 Y = dataset['clean_outcome']
 Y = Y.fillna(0)
-
-
 
 # Get 60% of the dataset as the training set. Put the remaining 40% in temporary variables: x_ and y_.
 x_train, x_, y_train, y_ = train_test_split(X, Y, test_size=0.40, random_state=1)
